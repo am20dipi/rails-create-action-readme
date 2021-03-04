255ecd5c59fb17f6a078766a34b85c0f2abc0fb0
Adriana <am20dipi@alum.siena.edu>
2021-03-04 17:19:36 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,16 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end
+  # (1) create a new Post instance
+  # (2) pass in the parameters from the form
+  # (3) save the record via .save
+  # (4) redirect to the newly-created post's show page
 end
